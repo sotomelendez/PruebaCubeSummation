7fd1a100222fd40aaf7714116f65737b3e805ef5
Ernesto Soto <e.soto162@uniandes.edu.co>
2016-03-10 04:46:34 +0000

    Ajuste titulo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,7 +16,7 @@
     	<div class="row">
     		<div class="col-md-12">
     			<h3 class="text-primary text-center">
-    				Prueba Cube Summation por Ernesto Soto
+    				Prueba Cube Summation
     			</h3>
     			
     			<br/>
